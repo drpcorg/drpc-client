4b8d6626038f89f3c7c06efa177a9461042f06ad
Termina1 <terminal2010@gmail.com>
2022-12-21 11:30:43 +0200

    fix var

--- tests/integration/crypto.spec.ts
+++ tests/integration/crypto.spec.ts
@@ -10,7 +10,7 @@ import {
 } from '../../src/isocrypto/util';
 
 let privKey: CryptoKey;
-var testKey =
+let testKey =
   '04b90fb62312ffdc6586d94736fb31abbb5fc1c2bcd26191417a2ca48657651b6c360d783f15a76d02a079934ae38e16e93e248fa9a5634488b0ddcd73a5bbdf3f';
 
 beforeAll(async () => {
