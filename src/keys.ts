cd9f49a76648f48271195f0b5dda8e72048ab35d
Konstantin Ryzhov <theaklair@gmail.com>
2023-03-02 01:51:49 +0600

    Added p2pvalidator key

--- src/keys.ts
+++ src/keys.ts
@@ -10,6 +10,7 @@ export type DrpcProviders =
   | 'attestant'
   | 'p-ops'
   | 'stakesquid'
+  | 'p2pvalidator'
   | 'test';
 
 let PUBLIC_KEYS: { [key in DrpcProviders]: string } = {
@@ -21,6 +22,8 @@ let PUBLIC_KEYS: { [key in DrpcProviders]: string } = {
     '0452c2216dac3ab4706a892760279d7141293b39fb081948188697d5487a987cb1968ccc300953f5c21bfe4553163b11eff39779392f4ce10f6b05792a7a596006',
   stakesquid:
     '044d40cb6f42e5a62dbf55fef6d137a8936d2fc105205b62a9af396a1d9cf0d0633ea1fbef9e16b7208e648e63858f5da1fe51ae879ef79a3b769d74995f810816',
+  p2pvalidator:
+    '04c028e39347bb0f75fa12c4f9525cf2b61d908fef9a68b8d9adce82433cd3c1e1b95522c5add42f2488da1d24cec77156633b057f8be9a15a6b1fa388b691dc9f',
   test: '04d0326a340b2590e37184a0fe4a3c95b8d4016a7196db46591ceb085adb203d062bc8c38758a9d7e38a66d752bd5871c1c005c802724aa6825f75adbca49582c8',
 };
 
